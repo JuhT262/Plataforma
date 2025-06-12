7cf888174e00822a8a74ca1a7c51fefa205049e7
JuhT262 <juliatrindade262@gmail.com>
2025-06-12 12:10:07 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -91,13 +91,13 @@ class Config:
     IMG_PROFILE = "https://i.ibb.co/vvD2dkbQ/17.png"
     IMG_GALLERY = [
         "https://i.ibb.co/vvD2dkbQ/17.png",
-        "https://i.ibb.co/Y4B7CbXf/IMG-9202.jpg",
-        "https://i.ibb.co/Fqf0gPPq/IMG-9199.jpg"
+        "https://i.ibb.co/1GcmQffP/11.png",
+        "https://i.ibb.co/rfK0DPBM/27.png"
     ]
     IMG_HOME_PREVIEWS = [
         "https://i.ibb.co/vvD2dkbQ/17.png",
-        "https://i.ibb.co/MxqKBk1X/Save-ClipApp-481825770-18486618637042608-2702272791254832108-n.jpg",
-        "https://i.ibb.co/F4CkkYTL/Save-ClipApp-461241348-1219420546053727-2357827070610318448-n.jpg"
+        "https://i.ibb.co/RpPBjFp1/image.png",
+        "https://i.ibb.co/DfTmwHZb/Swapfaces-AI-7b3f94e0-0b2d-4ca6-9e7f-4313b6de3499.png"
     ]
     LOGO_URL = "https://i.ibb.co/LX7x3tcB/Logo-Golden-Pepper-Letreiro-1.png"
 
