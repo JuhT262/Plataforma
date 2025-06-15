56f9cb1e443553f35de03418f2aae057f35bc20d
JuhT262 <juliatrindade262@gmail.com>
2025-06-15 12:50:24 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -82,7 +82,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 # ======================
 class Config:
 
-    API_KEY = "AIzaSyAaLYhdIJRpf_om9bDpqLpjJ57VmTyZO7g"
+    API_KEY = st.secrets.get("GEMINI_API_KEY", "AIzaSyAaLYhdIJRpf_om9bDpqLpjJ57VmTyZO7g")
     API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={API_KEY}"
     VIP_LINK = "https://exemplo.com/vip"
     CHECKOUT_PROMO = "https://pay.risepay.com.br/Pay/c7abdd05f91d43b9bbf54780d648d4f6"
