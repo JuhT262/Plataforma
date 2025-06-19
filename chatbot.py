5acc3246bae2fa497373d3a793e7499ac82b556f
JuhT262 <juliatrindade262@gmail.com>
2025-06-18 21:06:34 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -143,7 +143,7 @@ class Config:
     REQUEST_TIMEOUT = 30
     AUDIO_FILE = "https://github.com/JuhT262/Plataforma/raw/refs/heads/main/assets/Juh%20of.mp3"
     AUDIO_DURATION = 8
-    IMG_PROFILE = "https://i.ibb.co/ks5CNrDn/IMG-9256.jpg"
+    IMG_PROFILE = "https://i.ibb.co/zhwMhzvr/Swapfaces-AI-1d2f0d36-04ab-42a2-837c-08fe6c357656.png"
     IMG_GALLERY = ['https://i.ibb.co/zhNZL4FF/IMG-9198.jpg', 'https://i.ibb.co/Y4B7CbXf/IMG-9202.jpg', 'https://i.ibb.co/Fqf0gPPq/IMG-9199.jpg']
     IMG_HOME_PREVIEWS = ['https://i.ibb.co/k2MJg4XC/Save-ClipApp-412457343-378531441368078-7870326395110089440-n.jpg', 'https://i.ibb.co/MxqKBk1X/Save-ClipApp-481825770-18486618637042608-2702272791254832108-n.jpg', 'https://i.ibb.co/F4CkkYTL/Save-ClipApp-461241348-1219420546053727-2357827070610318448-n.jpg']
     LOGO_URL = "https://i.ibb.co/LX7x3tcB/Logo-Golden-Pepper-Letreiro-1.png"
