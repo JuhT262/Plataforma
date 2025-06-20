6b6834beb2f3ba3f1d5216186e5312d8f3185558
JuhT262 <juliatrindade262@gmail.com>
2025-06-20 14:28:52 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -751,7 +751,7 @@ class UiService:
                     margin: 20px 0;
                 }}
                 .sidebar-header img {{
-                    border-radius: 0 !important;
+                    border-radius: 50%
                     border: 2px solid #ff66b3;
                     width: 80px;
                     height: 80px;
