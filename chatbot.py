ec2147af08e9dadaeab7fc661202bf8662de31f1
JuhT262 <juliatrindade262@gmail.com>
2025-06-20 15:05:42 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -736,15 +736,17 @@ class UiService:
                     z-index: 1;
                     
                 }}
-                img.sidebar-logo {{
-                display: block;
-                width: 100% !important;
-                max-width: 1200px !important;  /* Aqui você controla o tamanho real */
-                height: auto !important;
-                object-fit: contain !important;
-                margin: 0 auto !important;
-                border: none !important;
-                border-radius: 0 !important;
+                .sidebar-logo {
+                    display: block;
+                    width: 100% !important;
+                    height: auto !important;
+                    object-fit: contain !important;
+                    margin: 0 auto !important;
+                    border: none !important;
+                    border-radius: 0 !important;
+                    padding: 10px 0 !important;
+                }
+
                 }}
                 @media (min-width: 768px) {{
                     img.sidebar-logo {{
