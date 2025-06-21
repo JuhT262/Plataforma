d16e6749c1e73649c406bf6a8c0d37067cc6da7f
JuhT262 <juliatrindade262@gmail.com>
2025-06-20 21:02:29 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1553,7 +1553,8 @@ class ChatService:
                 time.sleep(5)
                 placeholder.empty()
                 resposta = {
-                    "text": "Nada de Pix direto, gostoso... 💸 Aqui você entra no meu mundinho só escolhendo um dos meus planos 😈\n\nVem ver tudo que preparei pra te deixar louco 🔥",
+                    "text": "Nada de Pix direto, gostoso... 💸 Aqui você entra no meu mundinho só escolhendo um dos meus planos: Promo, Start, Premium e Extreme 😈  
+Vem ver tudo que preparei pra te deixar louco 🔥",
                     "cta": {
                         "show": True,
                         "label": "👉 Ver Planos VIP",        
